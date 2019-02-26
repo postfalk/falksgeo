7e7951e6b8567f178ad4494186e8c7aa5e0113bd
Falk Schuetzenmeister <falk.schuetzenmeister@tnc.org>
2019-02-26 13:30:12 -0800

    add hashing

--- falksgeo/bootstrap.py
+++ falksgeo/bootstrap.py
@@ -3,6 +3,8 @@ Bootstrap, ensure data sources
 """
 # standard library
 import os
+import json
+import hashlib
 import shutil
 from zipfile import ZipFile
 # third party
@@ -15,6 +17,9 @@ from .shapefile import gdb_to_shp, csv_to_shp
 from .earthengine import raster_download
 from .files import ensure_directory
 from . import bootstrap
+
+
+BLOCKSIZE = 65536
 
 
 class CreationError(Exception):
@@ -38,12 +43,82 @@ def unzip(zipf, dest, **kwargs):
         zf.extractall(os.path.split(zipf)[0])
 
 
+def hash_file(file_path):
+    """
+    Hash file to track changes
+    """
+    # see https://www.pythoncentral.io/hashing-files-with-python/
+    hasher = hashlib.md5()
+    with open(file_path, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
+    return hasher.hexdigest()
+
+
+def check_source_changes(
+        sources, hash_store_name=None, no_hash_store_default=False
+    ):
+    """
+    Track upstream source changes.
+
+    Args:
+        sources: lst(str) - List of upstream sources (filenames)
+        hash_store_name: str - Filename where to store hashes
+        no_has_store_default: boolean - Default return when no store present
+
+    Returns:
+        boolean: Whether upstream sources should be considered changed
+    """
+    ret = False
+    hash_dic = {}
+    hsh = ''
+    if not hash_store_name:
+        print('Not tracking changes')
+        ret = False
+    else:
+        try:
+            with open(hash_store_name) as fil:
+                hash_dic = json.loads(fil.read())
+        except FileNotFoundError:
+            print('Hash store does not exist: Overwrite results')
+            ret = True
+        if not isinstance(sources, (list, tuple)):
+            sources = [sources]
+        for item in sources:
+            try:
+                hsh = hash_dic[item]
+            except KeyError:
+                print('Key does not exist in hash file: Overwrite results')
+                ret = True
+            new_hash = hash_file(item)
+            if hsh != new_hash:
+                hash_dic.update({item: new_hash})
+                hash_store_new = json.dumps(hash_dic)
+                with open(hash_store_name, 'w') as fil:
+                    fil.write(hash_store_new)
+                ret = True
+                print('Upstream source changed')
+            else:
+                print('Sources did not change')
+    return ret
+
+
+def check_file_exists(file_path):
+    """
+    Check whether file exists
+    """
+    return os.path.isfile(file_path)
+
+
 def check_or_create_files(
         source_path,
         file_path,
         directory=None,
         create_function=copy_tree,
-        create_kwargs={}
+        create_kwargs={},
+        hash_store_name=None
     ):
     """
     Check whether a file exists, if not attempt creation by adding
@@ -53,7 +128,9 @@ def check_or_create_files(
         create_function=getattr(bootstrap, create_function)
     if directory:
         ensure_directory(directory)
-    if not os.path.isfile(file_path):
+    create = (
+        not check_file_exists(file_path) or check_source_changes(file_path))
+    if create:
         print(
             '\nAttempting creation of {}\nfrom {}\n'.format(
                 file_path, source_path))
@@ -70,7 +147,7 @@ def get_from_tuple(tpl, idx):
         pass
 
 
-def get_assets(list_of_assets):
+def get_assets(list_of_assets, hash_store_name=None):
     """
     Try to download and install data sources for a project. Provide source
     that can handled by the function. The destination should be a file that
@@ -87,6 +164,7 @@ def get_assets(list_of_assets):
         for ds in list_of_assets:
             check_or_create_files(
                 ds[0], ds[1], create_function=ds[2],
-                create_kwargs=get_from_tuple(ds, 3))
+                create_kwargs=get_from_tuple(ds, 3),
+                hash_store_name=hash_store_name)
     except CreationError:
         print('Dataset MISSING and creation FAILED\n')
--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -9,7 +9,9 @@ from .base import DirectoryTestCase, TEST_RES_DIR
 TEST_A_DIR = os.path.join(TEST_RES_DIR, 'adir')
 TEST_B_DIR = os.path.join(TEST_RES_DIR, 'bdir')
 TEST_FILENAME = os.path.join(TEST_A_DIR, 'testfile.csv')
+ANOTHER_TEST_FILENAME = os.path.join(TEST_A_DIR, 'anothertestfile.csv')
 TEST_RES_FILENAME = os.path.join(TEST_RES_DIR, 'bdir', 'testfile.csv')
+TEST_HASH_STORE = os.path.join(TEST_RES_DIR, 'hashes.json')
 
 
 class TestCheckOrCreateFile(DirectoryTestCase):
@@ -26,3 +28,43 @@ class TestCheckOrCreateFile(DirectoryTestCase):
         bootstrap.check_or_create_files(
             TEST_A_DIR, TEST_RES_FILENAME,
             create_kwargs={'directory': TEST_B_DIR})
+
+
+class TestHashing(DirectoryTestCase):
+
+    def moreSetUp(self):
+        ensure_directory(TEST_A_DIR)
+        with open(TEST_FILENAME, 'w') as tf:
+            tf.write('0,0,0,0')
+        with open(ANOTHER_TEST_FILENAME, 'w') as tf:
+            tf.write('bla')
+
+    def test_hash_file(self):
+        self.assertEqual(
+            bootstrap.hash_file(TEST_FILENAME),
+            '886364986cd9a5c816240f0512f36bee')
+        with open(TEST_FILENAME, 'a') as fil:
+            fil.write('test')
+        # make sure we hash the content and not the filename
+        self.assertEqual(
+            bootstrap.hash_file(TEST_FILENAME),
+            'd33cdfbc66e23a13a5844cbf12794352')
+
+    def test_source_changes(self):
+        # source change always True if no hash file provided
+        self.assertFalse(bootstrap.check_source_changes(TEST_FILENAME))
+        self.assertFalse(bootstrap.check_source_changes(TEST_FILENAME))
+        # source should be written to the hash file
+        self.assertTrue(bootstrap.check_source_changes(
+            TEST_FILENAME, hash_store_name=TEST_HASH_STORE))
+        self.assertFalse(bootstrap.check_source_changes(
+            TEST_FILENAME, hash_store_name=TEST_HASH_STORE))
+        test_list = [TEST_FILENAME, ANOTHER_TEST_FILENAME]
+        self.assertTrue(bootstrap.check_source_changes(
+            test_list, hash_store_name=TEST_HASH_STORE))
+        self.assertFalse(bootstrap.check_source_changes(
+            test_list, hash_store_name=TEST_HASH_STORE))
+        with open(ANOTHER_TEST_FILENAME, 'a') as fil:
+            fil.write('hello')
+        self.assertTrue(bootstrap.check_source_changes(
+            test_list, hash_store_name=TEST_HASH_STORE))
