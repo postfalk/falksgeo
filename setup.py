b8dfe3482e75601c18a7416cecf1b38e2514dc35
Falk Schuetzenmeister <falk.schuetzenmeister@tnc.org>
2021-03-16 11:52:36 -0700

    update dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,38 @@
+"""
+The module setup
+"""
+import subprocess
+import sys
 from setuptools import setup
+
+
+def get_installed_gdal():
+    """
+    Read the available GDAL version from the system and install
+    GDAL accordingly.
+
+    Returns:
+        str - the installed GDAL version as text or
+        None - if no version is found
+    """
+    res = subprocess.run(
+        ['gdalinfo', '--version'], capture_output=True, check=True)
+    parts = res.stdout.decode('utf-8').split(' ')
+    try:
+        return parts[1].strip(',')
+    except AttributeError:
+        return None
+
+
+gdal_version = get_installed_gdal()
+if not gdal_version:
+    print('Please, install GDAL on your system before proceeding.')
+    sys.exit(1)
 
 
 setup(
     name='falksgeo',
-    version='0.1',
+    version='0.1.1',
     description='Geoprocessing functions to be used in projects',
     url='https://github.com/postfalk/falksgeo',
     author='Falk Schuetzenmeister',
@@ -12,12 +41,16 @@ setup(
     packages=['falksgeo'],
     zip_safe=False,
     install_requires=[
-        'pyproj==1.9.6',
-        'arcgis==1.5.2.post1',
-        'geopandas==0.4.0',
-        'earthengine-api==0.1.164',
-        'oauth2client==4.1.3',
-        'rasterio==1.0.15',
-        'requests==2.21',
-        'tqdm==4.30']
+        'gdal=={}'.format(gdal_version),
+        'numpy>=1.18,<1.19',
+        'pyproj>=3.0.1',
+        'arcgis>=1.8',
+        'geopandas>=0.9',
+        'earthengine-api>=0.1.164',
+        'oauth2client>=4.1.3',
+        'rasterio>=1.2',
+        'requests>=2.25',
+        'tqdm>=4.59',
+        'nose'
+    ]
 )
